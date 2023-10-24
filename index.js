8bdc868b70615cdbbcb65c0bd52137c18450b001
sanyamk0 <sanyamkumar002@gmail.com>
2023-10-24 22:39:38 +0530

    created MVC and integrated with products, brands and categories

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
 const express = require("express");
 const server = express();
 const mongoose = require("mongoose");
+const productsRouter = require("./routes/Products");
+const brandsRouter = require("./routes/Brands");
+const categoriesRouter = require("./routes/Categories");
+const cors = require("cors");
+
+//Middlewares
+server.use(
+  cors({
+    exposedHeaders: ["X-Total-Count"],
+  })
+);
+server.use(express.json()); //to parse req.body
+server.use("/products", productsRouter.router);
+server.use("/brands", brandsRouter.router);
+server.use("/categories", categoriesRouter.router);
 
 main().catch((err) => console.log(err));
 
 async function main() {
-  await mongoose.connect("mongodb://127.0.0.1:27017/test");
+  await mongoose.connect("mongodb://127.0.0.1:27017/ecommerce");
   console.log("Database Connected");
 }
 
---
+++ model/Product.js
@@ -0,0 +1,42 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+const productSchema = new Schema({
+  title: { type: String, required: true, unique: true },
+  description: { type: String, required: true },
+  price: {
+    type: Number,
+    min: [1, "Wrong min price"],
+    max: [10000, "Wrong max price"],
+  },
+  discountPercentage: {
+    type: Number,
+    min: [1, "Wrong min discount"],
+    max: [99, "Wrong max discount"],
+  },
+  rating: {
+    type: Number,
+    min: [0, "Wrong min rating"],
+    max: [5, "Wrong max price"],
+    default: 0,
+  },
+  stock: { type: Number, min: [0, "Wrong min stock"], default: 0 },
+  brand: { type: String, required: true },
+  category: { type: String, required: true },
+  thumbnail: { type: String, required: true },
+  images: { type: [String], required: true },
+  deleted: { type: Boolean, default: false },
+});
+
+const virtual = productSchema.virtual("id");
+virtual.get(function () {
+  return this._id;
+});
+productSchema.set("toJSON", {
+  virtuals: true,
+  versionKey: false,
+  transform: function (doc, ret) {
+    delete ret._id;
+  },
+});
+
+exports.Product = mongoose.model("Product", productSchema);
