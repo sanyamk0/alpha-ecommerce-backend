8bb0919336d4dd3f132041b3c9204bf7dce996a1
sanyamk0 <sanyamkumar002@gmail.com>
2023-10-27 13:42:46 +0530

    fixed adminOrder related APIs

--- controller/Order.js
+++ controller/Order.js
@@ -10,10 +10,33 @@ exports.createOrder = async (req, res) => {
   }
 };
 
-exports.fetchOrdersByUser = async (req, res) => {
-  const { user } = req.query;
+exports.fetchAllOrders = async (req, res) => {
+  // sort = {_sort:"price",_order="desc"}
+  // pagination = {_page:1,_limit=10}
+  let query = Order.find({ deleted: { $ne: true } });
+  let totalOrdersQuery = Order.find({ deleted: { $ne: true } });
+  if (req.query._sort && req.query._order) {
+    query = query.sort({ [req.query._sort]: req.query._order });
+  }
+  const totalDocs = await totalOrdersQuery.count().exec();
+  if (req.query._page && req.query._limit) {
+    const pageSize = req.query._limit;
+    const page = req.query._page;
+    query = query.skip(pageSize * (page - 1)).limit(pageSize);
+  }
   try {
-    const orders = await Order.find({ user: user });
+    const docs = await query.exec();
+    res.set("X-Total-Count", totalDocs);
+    res.status(200).json(docs);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+};
+
+exports.fetchOrdersByUser = async (req, res) => {
+  const { userId } = req.params;
+  try {
+    const orders = await Order.find({ user: userId });
     res.status(200).json(orders);
   } catch (err) {
     res.status(400).json(err);
--- routes/Order.js
+++ routes/Order.js
@@ -4,13 +4,15 @@ const {
   fetchOrdersByUser,
   deleteOrder,
   updateOrder,
+  fetchAllOrders,
 } = require("../controller/Order");
 
 const router = express.Router();
 //  /orders is already added in base path
 router
   .post("/", createOrder)
-  .get("/", fetchOrdersByUser)
+  .get("/", fetchAllOrders)
+  .get("/user/:userId", fetchOrdersByUser)
   .patch("/:id", updateOrder)
   .delete("/:id", deleteOrder);
 
